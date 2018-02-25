6d94afcc3ca6997be641a4ed0c38a4a288117d85
Ryan Barnett <ryan@trackmaven.com>
2018-02-25 15:32:34 -0500

    eslint airbnb and prettier

--- src/App.js
+++ src/App.js
@@ -1,60 +1,43 @@
-import React, { Component } from 'react';
-import {
-  Helmet
-} from 'react-helmet';
+import React from 'react';
+import {Helmet} from 'react-helmet';
 import styled from 'styled-components';
-import {
-  Switch,
-  Route
-} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
 
-
-const AppWrapper = styled.div `
+const AppWrapper = styled.div`
   margin: 0 auto;
   display: flex;
   min-height: 100%;
   flex-direction: column;
 `;
 
-const Main = () => 
-  <p className = "App-intro" >
-    To get started, edit <code > src / App.js </code> and save to reload. 
+const Main = () => (
+  <p className="App-intro">
+    To get started, edit <code> src / App.js </code> and save to reload.
   </p>
+);
 
-const Feature = () => <div>Feature</div>
-  
-class App extends Component {
-  render() {
-    return (
-      <AppWrapper className="App">
-        <Helmet
-          titleTemplate="%s - React-O-Matic"
-          defaultTitle="React-O-Matic"
-        >
-          <meta name="description" content="Sensible extensions to create-react-app" />
-        </Helmet>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-              <Switch>
-                <Route exact path = "/"
-              component = {
-                Main
-              }/> 
-              <Route path = "/feature"
-              component = {
-                Feature
-              }
-              /> 
-            </Switch>
+const Feature = () => <div>Feature</div>;
 
-      </AppWrapper>
-    );
-  }
-}
+const App = () => (
+  <AppWrapper className="App">
+    <Helmet titleTemplate="%s - React-O-Matic" defaultTitle="React-O-Matic">
+      <meta
+        name="description"
+        content="Sensible extensions to create-react-app"
+      />
+    </Helmet>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route path="/feature" component={Feature} />
+    </Switch>
+  </AppWrapper>
+);
 
 export default App;
